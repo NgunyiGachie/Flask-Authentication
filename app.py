106012820ec90a163ca935428e5ab349e1f3b995
NgunyiGachie <antogachie@gmail.com>
2025-03-10 21:46:07 +0300

    migrations

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db, User, Post, Role
 
 app = Flask(__name__)
 app.secret_key = b'Y].D\xa3\x1e\xf9\xc40\xc5\x8e9\xce\x1d\x81\xedF/\xef\xd8\xed:\xef\xff'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
