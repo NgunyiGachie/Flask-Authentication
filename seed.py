00c980e8bb559614c8dcb5582333594ab18e0f9e
NgunyiGachie <antogachie@gmail.com>
2025-03-10 21:06:41 +0300

    seed data

--- seed.py
+++ seed.py
@@ -1,7 +1,7 @@
 from random import randint, choice as rc
 from faker import Faker
 from app import app
-from models import db, User, Post
+from models import db, User, Post, Role
 
 faker = Faker()
 
@@ -9,14 +9,49 @@ with app.app_context():
     print("Deleting all records...")
     User.query.delete()
     Post.query.delete()
+    Role.query.delete()
 
     fake = Faker()
 
+    print("Creating roles....")
+    roles = ['Admin', "Editor", "User"]
+    role_objects = [Role(name=role) for role in roles]
+    db.session.add_all(role_objects)
+    db.session.commit()
+
     print("Creating users....")
     users = []
-    usernames = []
-    emails = []
+    usernames = set()
+    emails = set()
 
-    for i in range(25):
-        username = fake.first_name()
-        
+    for _ in range(25):
+        username = faker.unique.first_name()
+        email = faker.unique.email()
+        role = rc(role_objects)
+
+        user = User(
+            username=username,
+            email=email,
+            password_hash=faker.password(),
+            role_id=role.id
+        )
+        users.append(user)
+
+    db.session.add_all(users)
+    db.session.commit()
+
+    print("Creating posts...")
+    posts = []
+
+    for _ in range(50):
+        post = Post(
+            title=faker.sentence(),
+            content=faker.text(),
+            author_id=rc(users).id
+        )
+        posts.append(post)
+
+    db.session.add_all(posts)
+    db.session.commit()
+
+    print("seedinf complete!")
